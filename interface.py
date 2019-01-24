d1644b1a19a945621e923d49e3ab6cd418f767cf
bechtold_michel <bechtold.michel@gmail.com>
2019-01-24 15:45:11 +0100

    include new format of DEFAULT_inputs_ensupd

--- interface.py
+++ interface.py
@@ -110,13 +110,49 @@ class LDAS_io(object):
         n_lines = len(lines)
 
         # 30 or 32 fields (before and after two entries for the use of uncertainty maps)
-        n_fields = 32 if n_lines == 128 else 30
+        if n_lines == 128 or n_lines == 896:
+            n_fields = 32
+        else:
+            n_fields = 30
 
         n_blocks = n_lines / n_fields
 
         res = []
         for bl in np.arange(n_blocks) * n_fields:
-            if n_fields == 32:
+            if n_fields == 32 and n_blocks == 28:
+                res.append({'descr': s(lines[bl + 0]),
+                            'species': int(lines[bl + 1]),
+                            'orbit': int(lines[bl + 2]),
+                            'pol': int(lines[bl + 3]),
+                            'N_ang': int(lines[bl + 4]),
+                            'ang': float(lines[bl + 5]),
+                            'freq': float(lines[bl + 6]),
+                            'FOV': float(lines[bl + 7]),
+                            'FOV_units': s(lines[bl + 8]),
+                            'assim': b(lines[bl + 9]),
+                            'scale': b(lines[bl + 10]),
+                            'getinnov': b(lines[bl + 11]),
+                            'RTM_ID': int(lines[bl + 12]),
+                            'bias_Npar': int(lines[bl + 13]),
+                            'bias_trel': int(lines[bl + 14]),
+                            'bias_tcut': int(lines[bl + 15]),
+                            'nodata': float(lines[bl + 16]),
+                            'varname': s(lines[bl + 17]),
+                            'units': s(lines[bl + 18]),
+                            'path': s(lines[bl + 19]),
+                            'name': s(lines[bl + 20]),
+                            'scalepath': s(lines[bl + 21]),
+                            'scalename': s(lines[bl + 22]),
+                            'flistpath': s(lines[bl + 23]),
+                            'flistname': s(lines[bl + 24]),
+                            'errstd': float(lines[bl + 25]),
+                            'std_normal_max': float(lines[bl + 26]),
+                            'zeromean': b(lines[bl + 27]),
+                            'coarsen_pert': b(lines[bl + 28]),
+                            'xcorr': float(lines[bl + 29]),
+                            'ycorr': float(lines[bl + 30]),
+                            'adapt': int(lines[bl + 31])})
+            elif n_fields == 32 and n_blocks == 4:
                 res.append({'descr': s(lines[bl + 0]),
                             'species': int(lines[bl + 1]),
                             'orbit': int(lines[bl + 2]),
