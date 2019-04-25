71d1deabbd612efecfe5e6a11b82b089f86f26d3
Michel Bechtold <michel.bechtold@kuleuven.be>
2019-04-25 14:06:36 +0200

    read_obsparam except for model-only (no DA) runs

--- interface.py
+++ interface.py
@@ -66,7 +66,10 @@ class LDAS_io(object):
 
         self.paths = paths(exp=exp, domain=domain)
 
-        self.obsparam = self.read_obsparam()
+        try:
+            self.obsparam = self.read_obsparam()
+        except:
+            'No obsparam file. This is a model-only run.'
 
         tilecoord = self.read_params('tilecoord')
         tilegrids = self.read_params('tilegrids')
