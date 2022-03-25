be68a40075716a66fadca124329ebf57199d9809
Alexander Gruber <reddy.gruber@icloud.com>
2022-03-25 10:27:09 +0100

    updated batchscripts

---
+++ batchscripts/create_netcdf_images_LDASsa.py
@@ -0,0 +1,17 @@
+
+import sys
+import getpass
+
+# This needs to point to the directory where pyldas is located (intended for the HPC)
+uid = getpass.getuser()
+sys.path.append('/data/leuven/' + uid[3:6] + '/' + uid + '/python')
+
+from pyldas.interface import LDASsa_io
+
+# experiment name and parameters to be converted to netCDF need to be passed when calling this script!
+root = sys.argv[1]
+exp = sys.argv[2]
+domain = sys.argv[3]
+for param in sys.argv[4::]:
+    io = LDASsa_io(param=param, root=root, exp=exp, domain=domain)
+    io.bin2netcdf(overwrite=True)
